4c3154765a2bac1670222e65131fd8155022e56a
Mo Ghanbari <mostafa.qanbary@gmail.com>
2020-06-29 21:19:44 -0400

    Created LOGOUT action

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types'
 import setAuthToken from '../../utils/setAuthToken'
 
@@ -92,4 +93,9 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       type: REGISTER_FAIL,
     })
   }
+}
+
+// Logout
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT })
 }
