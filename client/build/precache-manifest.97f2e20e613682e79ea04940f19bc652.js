c328f7e946f9185584225d4a7f284690bd13cb8f
Mo Ghanbari <mostafa.qanbary@gmail.com>
2020-07-03 12:52:48 -0400

    upload create-react-app build folder to test netlify
    You can't change default build folder directory in create-react-app!!

---
+++ client/build/precache-manifest.97f2e20e613682e79ea04940f19bc652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e0eefc6fda5eb09d5b65ad0d1587ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cfcdaacbc0d2fef6527",
+    "url": "/static/css/main.bb53614c.chunk.css"
+  },
+  {
+    "revision": "74a310e869457c0d711e",
+    "url": "/static/js/2.b5543ade.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5543ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfcdaacbc0d2fef6527",
+    "url": "/static/js/main.93179cd9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b319374854cb03c00bfdb0a25ef118ef",
+    "url": "/static/media/landing-background.b3193748.jpg"
+  }
+]);
