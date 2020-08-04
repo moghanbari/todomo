630d8a1da6d118a050487aa5083b41a868fa6f80
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-08-04 00:41:47 -0400

    removed default.json which was not neccessary

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97f2e20e613682e79ea04940f19bc652.js"
+  "/precache-manifest.254b628392ca9f68f952e17e6dcef79f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
