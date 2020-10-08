c6253f875fca6b44a56e98831b3af83a787f774c
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-08 16:08:32 -0400

    Cleaned up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
 import Footer from './components/layouts/Footer'
 import Landing from './components/pages/Landing'
+import Routes from './components/routing/Routes'
 
 // Redux
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { loadUser } from './redux/actions/auth'
-import Routes from './components/routing/Routes'
 
 import './assets/css/reset.css'
 import './assets/css/style.css'
@@ -24,7 +24,7 @@ const App = () => {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Landing} />
-          <Routes component={Routes} />
+          <Routes />
         </Switch>
         <Footer />
       </Router>
