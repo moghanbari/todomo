dd095cd9a426ce472250e98a34cbc60f4d43851a
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-08 11:56:30 -0400

    Devided "require" statements in a categorized way for consistency and clearity

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../../middleware/auth')
+
+const config = require('../../config')
+
 const { check, validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
-const config = require('../../config')
 const jwt = require('jsonwebtoken')
 
+const auth = require('../../middleware/auth')
+
 const User = require('../../models/User')
+
 const { JWT_SECRET } = config
 
 /**
