33e26fbcedffda8d48e62fcaa2217f313196f8b6
Mo Ghanbari <mo.ghanbari@icloud.com>
2021-06-10 13:03:20 -0400

    Fixed warnings due to react version update to be able to deploy

--- client/src/components/layouts/Alert.js
+++ client/src/components/layouts/Alert.js
@@ -1,25 +1,18 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-const Alert = ({ alerts }) =>
-  alerts !== null &&
-  alerts.length > 0 &&
-  alerts.map((alert) => (
-    <div
-      key={alert.id}
-      className={`mt-100 container alert alert-${alert.alertType}`}
-    >
-      <p>{alert.msg}</p>
-    </div>
-  ))
+const Alert = () => {
+  const alerts = useSelector((state) => state.alert)
 
-Alert.propTypes = {
-  alerts: PropTypes.array.isRequired,
+  return (
+    alerts !== null &&
+    alerts.length > 0 &&
+    alerts.map((alert) => (
+      <div key={alert.id} className={`mt-100 container alert alert-${alert.alertType}`}>
+        <p>{alert.msg}</p>
+      </div>
+    ))
+  )
 }
 
-const mapStateToProps = (state) => ({
-  alerts: state.alert,
-})
-
-export default connect(mapStateToProps)(Alert)
+export default Alert
